3796cdec8c9657ead53c250f50f49e4a2b6ccd33
yexiaoyong <443740233@qq.com>
2019-04-24 14:07:13 +0800

    commit code

---
+++ Sample/Sample/Controllers/ContentController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Sample.Models;
+
+namespace Sample.Controllers
+{
+    public class ContentController : Controller
+    {
+        private readonly List<Content> contents;
+        public ContentController(IOptions<List<Content>> option)
+        {
+            contents = option.Value;
+        }
+        public IActionResult Index()
+        {
+            //var contentList = new List<Content>();
+            //for (var i = 0; i < 11; i++)
+            //{
+            //    contentList.Add(new Content()
+            //    {
+            //        Id = i,
+            //        title = i.ToString() + "的标题",
+            //        content = i.ToString() + "的内容",
+            //        status = 1,
+            //        createtime = DateTime.Now.AddDays(-i)
+            //    });
+            //}
+            return View(new ContentViewModel() { Contents = contents });
+        }
+    }
+}
---
+++ Sample/Sample/Models/Content.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sample.Models
+{
+    public class Content
+    {
+        /// <summary>
+        /// 主键ID
+        /// </summary>
+        public int? Id { get; set; }
+        /// <summary>
+        /// 标题
+        /// </summary>
+        public string title { get; set; }
+        /// <summary>
+        /// 内容
+        /// </summary>
+        public string content { get; set; }
+        /// <summary>
+        /// 状态1:正常0:删除
+        /// </summary>
+        public int? status { get; set; }
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime? createtime { get; set; }
+        /// <summary>
+        /// 修改时间
+        /// </summary>
+        public DateTime? edittime { get; set; }
+
+    }
+}
